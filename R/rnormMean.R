8869745604508959df5a3b98490f253c265a09a0
Nancy Irisarri Mendez <nancy.irisarrimendez@mrdm.nl>
2018-03-15 11:28:02 +0100

    Add @export annotation.

--- R/rnormMean.R
+++ R/rnormMean.R
@@ -1,7 +1,9 @@
 #' A test function
 #'
 #' And this just a brief test documentation.
-
+#'
+#' @export
+#' @param samples number of samples
 normalMean <- function(samples=10){
   data <- rnorm(samples)
   mean(data)
